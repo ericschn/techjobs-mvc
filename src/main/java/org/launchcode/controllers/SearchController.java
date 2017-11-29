01f3eb658aaef1cd61ddb852d96e9c11f26321e5
ericschn <eschneider327@gmail.com>
2017-11-29 15:39:26 -0600

    Base project done, yet to finish bonus missions

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -16,12 +17,39 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
+    private ArrayList<HashMap<String, String>> jobList;
+
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
+        // model.addAttribute("jobs", jobList);
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "/results")
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        // Selecting search type
+
+        if (searchType.equals("all")) {
+            jobList = JobData.findByValue(searchTerm);
+        } else {
+            jobList = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        // Count results
+
+        int numJobs = 0;
+        if (!jobList.isEmpty()) {
+            numJobs = jobList.size();
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobList);
+        model.addAttribute("numJobs", numJobs);
+        return "search";
+    }
 
 }
